ad368e188e89fa0df4e9dd636fa272563cc0731b
kao <kaouthar.lefkir@gmail.com>
2025-05-22 14:03:03 +0100

    edit for alerts tasks

--- backend/PetroMonitore/apps.py
+++ backend/PetroMonitore/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class PetromonitoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "PetroMonitore"
+    
+    
+    def ready(self):
+        # Force l'import de tasks au démarrage de Django
+        import PetroMonitore.alerts.tasks
